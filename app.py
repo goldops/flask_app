bdc8dcd2b86794a692379ab81371f5c10a35843a
goldops <nmn.ncls@gmail.com>
2025-05-23 11:12:34 +0000

    adding POST route and fields verification

--- app.py
+++ app.py
@@ -1,14 +1,46 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+cart =[]
 
 @app.route('/')
 def hello_world():
     return "Coucou !"
 
-@app.route('/cart')
-def cart():
-    return "Panier vide !"
+@app.route('/cart', methods=['GET'])
+def list_cart():
+    return jsonify(cart), 200
+
+@app.route('/cart', methods=['POST'])
+def add_to_cart():
+    try:
+        body = request.get_json()
+        if 'id' not in body.keys() or 'quantity' not in body.keys():
+            return jsonify({'error': "Missing fields."}), 400
+        
+        for i, item in enumerate(cart):
+            if item['id'] == body['id']:
+                cart[i]['quantity'] += int(body['quantity'])
+                return jsonify({}), 200
+        
+        cart.append(body)
+        return jsonify({}), 200
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+def check_fields(body, fields):
+    required_parameters_set = set(fields)
+    fields_set = set(body.keys())
+    return required_parameters_set <= fields_set
+
+print(check_fields({}, {'id', 'quantity'}))
+print(check_fields({'id':0}, {'id', 'quantity'}))
+print(check_fields({'id': 0, 'quantity': 0}, {'id', 'quantity'}))
+print(check_fields({'id':0, 'quantity':0, 'description':""}, {'id', 'quantity'}))
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
