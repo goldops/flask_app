c5955595c5ffeb52cddd9048180a2274fa199ee6
goldops <nmn.ncls@gmail.com>
2025-05-23 12:25:10 +0000

    adding patch requests

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import requests
+import requests, jsonify
 
 print(requests.get("http://127.0.0.1:5000").content.decode())
 print(requests.get("http://127.0.0.1:5000").content.decode())
@@ -7,3 +7,45 @@ print(check_fields({}, {'id', 'quantity'}))
 print(check_fields({'id':0}, {'id', 'quantity'}))
 print(check_fields({'id': 0, 'quantity': 0}, {'id', 'quantity'}))
 print(check_fields({'id':0, 'quantity':0, 'description':""}, {'id', 'quantity'}))
+
+req = requests.post("http://127.0.0.1:5000/cart", json={
+    'id': "azeezrzerd"
+})
+print(req.status_code, req.json())
+
+
+req = requests.post("http://127.0.0.1:5000/cart", json={
+    'id' : "qefsderf",
+    'quantity': 3
+})
+print(req.status_code, req.json())
+
+
+req = request.get("http://127.0.0.1:5000/cart",).json()
+print(req)
+
+
+requests.post("http://127.0.0.1:5000/cart", json={
+    'id' : "aqasdvb",
+    'quantity': 1
+})
+
+requests.post("http://127.0.0.1:5000/cart", json={
+    'id' : "aqasdvb",
+    'quantity': 3
+})
+
+req = request.get("http://127.0.0.1:5000/cart",).json()
+print(req)
+
+req = requests.patch("http://127.0.0.1:5000/cart", json={
+    'id': "aaaaa",
+    'quantity': 10
+})
+print(req.status_code, req.json())
+
+req = requests.patch("http://127.0.0.1:5000/cart", json={
+    'id': "je8zng",
+    'quantity': 10
+})
+print(requests.get("http://127.0.0.1:5000/cart").json())
