1177c2f5867955e729c8ecade0e78e72f9116857
goldops <nmn.ncls@gmail.com>
2025-05-23 10:28:44 +0000

    adding tests for routing checks

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return "Coucou !"
 
+@app.route('/cart')
+def cart():
+    return "Parnier vide !"
+
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ test.py
@@ -0,0 +1,4 @@
+import requests
+
+print(requests.get("http://127.0.0.1:5000").content.decode())
+print(requests.get("http://127.0.0.1:5000").content.decode())
